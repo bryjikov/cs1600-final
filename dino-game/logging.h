02414da9bd29ece2a5f80bd5b92ac71ad6b8bb5a
Thomas Castleman <thomascastleman0@gmail.com>
2021-12-02 09:51:55 -0500

    fix function name in debug/error logging

--- dino-game/logging.h
+++ dino-game/logging.h
@@ -29,26 +29,40 @@ void serial_printf(char *format, ...)
 }
 
 /*
+   Macro wrapper around halt_with_error_underlying so we can call it
+   with the appropriate value of __func__
+*/
+#define halt_with_error(format, ...) \
+  halt_with_error_underlying(__func__, format, ##__VA_ARGS__)
+
+/*
    Print an error message (possibly using a format string),
    and then halt the system (loop forever).
 */
-void halt_with_error(char *format, ...)
+void halt_with_error_underlying(char *func, char *format, ...)
 {
   char buf[LOG_BUFFER_SIZE];
   FORMAT_TO_BUFFER(buf, LOG_BUFFER_SIZE, format);
-  serial_printf("[ERROR] %s: %s\n", __func__, buf);
+  serial_printf("[ERROR] %s: %s\n", func, buf);
   while (true);
 }
+
+/*
+   Macro wrapper around debug_underlying so we can call it
+   with the appropriate value of __func__
+*/
+#define debug(format, ...) \
+  debug_underlying(__func__, format, ##__VA_ARGS__)
 
 // Print a debug message (possibly using a format string). This only
 // does anything if DEBUG_LOGGING is defined; otherwise, it is a no-op.
 #ifdef DEBUG_LOGGING
-void debug(char *format, ...)
+void debug_underlying(char *func, char *format, ...)
 {
   char buf[LOG_BUFFER_SIZE];
   FORMAT_TO_BUFFER(buf, LOG_BUFFER_SIZE, format);
-  serial_printf("[DEBUG] %s: %s\n", __func__, buf);
+  serial_printf("[DEBUG] %s: %s\n", func, buf);
 }
 #else
-void debug(char *format, ...) {}
+void debug_underlying(char *format, ...) {}
 #endif
