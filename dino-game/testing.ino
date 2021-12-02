97821722966ff7087e72097b0eade614cb188a3c
Casey <casey_nelson@brown.edu>
2021-12-02 18:00:21 -0500

    test function

--- dino-game/testing.ino
+++ dino-game/testing.ino
@@ -15,6 +15,52 @@ typedef struct {
   unsigned long start_time;                /* The time (in milis) when the player begins */
   unsigned long duration;                  /* The total time that the game lasted */
   LinkedPointerList<obstacle_t> *all_obstacles;     /* List containing all currently active obstacles */
+  state_t current_state;
 } state_vars;
+
+
+
+bool run_one_test(state_vars start_state, state_vars expected_state, unsigned long current_millis){
+  //set global variables to match desired input 
+  game_over_flag = start_state.game_over_flag;
+  pre_direction_change_flag = start_state.pre_direction_change_flag;
+  time_entered_pdc = start_state.time_entered_pdc;         
+  restart_flag = start_state.restart_flag;                       
+  obstacle_move_interval = start_state.obstacle_move_interval;        
+  time_last_obstacle_move = start_state.time_last_obstacle_move;   
+  time_last_dir_chg = start_state.time_last_dir_chg;        
+  time_last_speed_up = start_state.time_last_speed_up;        
+  time_entered_setup = start_state.time_entered_setup;       
+  obstacle_direction = start_state.obstacle_direction;          
+  player_x = start_state.player_x;                           
+  player_y = start_state.player_y;                            
+  moved = start_state.moved;                             
+  start_time = start_state.start_time;               
+  duration = start_state.duration;                  
+  all_obstacles = start_state.all_obstacles;  
+  current_state = start_state.current_state;
+  // run fsm
+  state_t end_state = update_game_state(current_millis);   
+  //check if end state macthes expected 
+  bool test_passed = (end_state == expected_state.current_state and  
+                      game_over_flag == expected_state.game_over_flag and
+                      pre_direction_change_flag == expected_state.pre_direction_change_flag and
+                      time_entered_pdc == expected_state.time_entered_pdc and         
+                      restart_flag == expected_state.restart_flag and                       
+                      obstacle_move_interval == expected_state.obstacle_move_interval and        
+                      time_last_obstacle_move == expected_state.time_last_obstacle_move and   
+                      time_last_dir_chg == expected_state.time_last_dir_chg and        
+                      time_last_speed_up == expected_state.time_last_speed_up and        
+                      time_entered_setup == expected_state.time_entered_setup and       
+                      obstacle_direction == expected_state.obstacle_direction and          
+                      player_x == expected_state.player_x and                           
+                      player_y == expected_state.player_y and                            
+                      moved == expected_state.moved and                             
+                      start_time == expected_state.start_time and               
+                      duration == expected_state.duration and                  
+                      all_obstacles == expected_state.all_obstacles);
+
+   return test_passed;                      
+}
 
 ​
